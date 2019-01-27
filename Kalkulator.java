667725c0c5acb3f42428978bc9514da7a27415e2
Seweryn Krupiński <45992679+sewerynkrupinski@users.noreply.github.com>
2019-01-27 12:48:47 +0100

    Kalkulator 3-liczby
    Kalkulator.java + Run.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -2,31 +2,50 @@ package J25_2;
 
 public class Kalkulator {
 
-    public double suma(double liczba1, double liczba2){
-
-        return liczba1 + liczba2;
-
-        //return suma;
+    public int suma(int a, int b){
+       return a+b;
     }
 
-    public double roznica(double liczba1, double liczba2){
-
-        double roznica = liczba1 - liczba2;
-
-        return roznica;
+    public int suma(int a, int b, int c){
+        return a + b + c;
     }
 
-    public double iloczyn(double liczba1, double liczba2){
-
-        double iloczyn = liczba1 * liczba2;
-
-        return iloczyn;
+    public int roznica(int a, int b){
+        return a-b;
     }
 
-    public double iloraz(double liczba1, double liczba2){
-
-        double iloraz = liczba1 / liczba2;
-
-        return iloraz;
+    public int roznica(int a, int b, int c){
+        return a - b - c;
     }
+
+    public int iloczyn(int a, int b){
+        return a*b;
+    }
+
+    public int iloczyn(int a, int b, int c){
+        return a * b * c;
+    }
+
+    public double iloraz(int a, int b){
+
+        if (b == 0){
+            System.out.println("nie można dzielić przez zero");
+            return 0;
+        }
+
+        else return a/b;
+    }
+
+    public double iloraz(int a, int b, int c){
+
+        if (b == 0 || c == 0) {
+            System.out.println("nie można dzielić przez zero");
+            return 0;
+        }
+        else return a / b / c;
+    }
+
+
+
+
 }
--- Run.java
+++ Run.java
@@ -1,5 +1,6 @@
 package J25_2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Run {
@@ -7,64 +8,101 @@ public class Run {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        double liczba1;
-        double liczba2;
+        ArrayList<Integer> liczby = new ArrayList<>();
+
         String znak;
         String dec;
+        int ile;
 
         Kalkulator obj = new Kalkulator();
 
         System.out.println("Witaj w programie kalkulator!");
         System.out.println();
 
-        while(true){ //wykonuje się dopóki nie wciśniemy "n" po wyjświetleniu wyniku
-            System.out.println("Podaj pierwszą liczbę");
-            liczba1 = input.nextDouble();
+        while(true){
+        System.out.println("Podaj liczbę elementów");
+        ile = input.nextInt();
+
+        if (ile >=4) {
+            System.out.println("Mogę liczyć dla max 3 liczb");
+            continue;
+
+        }
+
+        else if (ile == 0) {
+            System.out.println("Nie podałeś żadnych liczb");
+            continue;
+        }
+
+
+
+        else {
+            for (int i = 1; i <= ile; i++) {
+                System.out.println("Podaj " + i + " liczbę");
+                liczby.add(input.nextInt());
+
+            }
+
             input.nextLine();
-            System.out.println("Podaj znak działania ( +, -, *, / ");
+
+            if (ile == 1) {
+                System.out.println("Twoja liczba to " + liczby.get(0));
+                liczby.clear();
+                continue;
+            }
+
+            System.out.println("Podaj znak działania ( +, -, *, / )");
             znak = input.nextLine();
-                while (!(znak.equals("+") || znak.equals("-") || znak.equals("*") || znak.equals("/"))){  //wykonuje się dopóki nie jest podany poprawny znak +, -, / lub *
-                    System.out.println("Niewłaściwy znak");
-                    System.out.println("Podaj znak działania ( +, -, *, / ");
-                    znak = input.nextLine(); //odczytuje znak, jeśli jest prawidłowy wychodzi z pętli while
+        }
+
+            if (znak.equals("/") && (ile==2 || ile ==3) && (liczby.get(1) == 0 || liczby.get(2) ==0)){
+                System.out.println("Nie możesz dzielić przez zero, podaj nowe liczby");
+
+                for (int i = 2; i <= ile; i++) {
+                    System.out.println("Podaj " + i + " liczbę");
+                    liczby.set(i-1, input.nextInt());
                 }
-
-            System.out.println("Podaj drugą liczbę");
-            liczba2 = input.nextDouble();
-            input.nextLine(); //odczytuje drugą liczbę
-
-
-            if (znak.equals("+")){
-                System.out.println("Suma twoich liczb to: " +obj.suma(liczba1,liczba2));
-
+                input.nextLine();
             }
 
-            else if (znak.equals("-")){
-                System.out.println("Różnica twoich liczb to: " +obj.roznica(liczba1,liczba2));
+            if (znak.equals("+") && liczby.size() == 2){
+                System.out.println("Suma twoich liczb to: " +obj.suma(liczby.get(0),liczby.get(1)));
 
             }
-            else if (znak.equals("*")){
-                System.out.println("Iloczyn twoich liczb to: " +obj.iloczyn(liczba1,liczba2));
-
+            else if (znak.equals("+") && liczby.size() == 3) {
+                System.out.println("Suma twoich liczb to: " + obj.suma(liczby.get(0), liczby.get(1), liczby.get(2)));
             }
 
-            else if (znak.equals("/")){
-                while (liczba2 == 0){ //jeżeli podana wcześniej liczba jest równa 0 a znakiem było dzielenie to wykonuje się dopóki nie podamy 2 liczby różnej od zera
-                    System.out.println("Nie można dzielić przez zero");
-                    System.out.println();
-                    System.out.println("Podaj drugą liczbę różną od zera");
-                    liczba2 = input.nextDouble();
-                    input.nextLine();
-                 }
-
-                System.out.println("Iloraz twoich liczb to: " + obj.iloraz(liczba1, liczba2));
+            if (znak.equals("-") && liczby.size() == 2){
+                System.out.println("Różnica twoich liczb to: " +obj.roznica(liczby.get(0),liczby.get(1)));
 
             }
+            else if (znak.equals("-") && liczby.size() == 3) {
+                System.out.println("Różnica twoich liczb to: " + obj.roznica(liczby.get(0), liczby.get(1), liczby.get(2)));
+            }
+
+            if (znak.equals("*") && liczby.size() == 2){
+                System.out.println("Iloczyn twoich liczb to: " +obj.iloczyn(liczby.get(0),liczby.get(1)));
+
+            }
+            else if (znak.equals("*") && liczby.size() == 3) {
+                System.out.println("Iloczyn twoich liczb to: " + obj.iloczyn(liczby.get(0), liczby.get(1), liczby.get(2)));
+            }
+
+            if (znak.equals("/") && liczby.size() == 2){
+                System.out.println("Iloraz twoich liczb to: " +obj.iloraz(liczby.get(0),liczby.get(1)));
+
+            }
+            else if (znak.equals("/") && liczby.size() == 3) {
+                System.out.println("Iloraz twoich liczb to: " + obj.iloraz(liczby.get(0), liczby.get(1), liczby.get(2)));
+            }
+
 
             System.out.println("Czy chesz dokonać kolejnych obliczeń? (T/N)");
             dec = input.nextLine().toUpperCase();
             if (dec.equals("T")){       //jeżeli wybierzemy "t" to wychodzi z aktualnej pętli i program wykonuje się ponownie
-                continue;
+                liczby.clear();
+                //continue;
             }
             else if (dec.equals("N")){      //jeżeli wybierzemy "n" to program kończy działanie
                 System.out.println("Do zobaczenia!");
